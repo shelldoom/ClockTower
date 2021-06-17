53925c7cf24b4c69592ad7e48835f879521ef6b6
shelldoom <virtualized4ver@gmail.com>
2021-06-17 13:43:54 +0530

    improvements

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ from telegram.ext import Updater, Dispatcher
 from config import botToken
 import setup
 
-updater: Updater = Updater(token=botToken)
-dispatcher: Dispatcher = updater.dispatcher
+def main():
+    updater: Updater = Updater(token=botToken)
+    dispatcher: Dispatcher = updater.dispatcher
 
-setup.setup(dispatcher)
+    setup.setup(dispatcher)
 
-updater.start_polling()
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == "__main__":
+    main()
