2c6f7a69f7a315f649915e0909efe580f0671ee3
shelldoom <virtualized4ver@gmail.com>
2021-06-17 13:47:03 +0530

    added quotable

--- components/quote.py
+++ components/quote.py
@@ -1,18 +1,23 @@
-from random import choice
+import json
 from telegram import Update
 from telegram.ext import CallbackContext, Filters, CommandHandler
+import requests
 
-QUOTES = [
-    '<i>What is better - to be born good, or to overcome your evil nature through great effort?</i> --<b>Paarthurnax</b>',   
-]
+# QUOTES = [
+#     '<i>What is better - to be born good, or to overcome your evil nature through great effort?</i> --<b>Paarthurnax</b>',
+#     ''
+# ]
+
 
 def get_quote() -> str:
-    return choice(QUOTES)
+    r = requests.get('https://api.quotable.io/random')
+    r = json.loads(r.text)
+    return f"<i>{r['content']}</i> --<b>{r['author']}</b>"
 
 def quote_callback(update:Update, context:CallbackContext):
     context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text=choice(QUOTES),
+        text=get_quote(),
         parse_mode='HTML'
     )
 quote_handler = CommandHandler(["quote", "quotes"], quote_callback, filters=~Filters.update.edited_message)
