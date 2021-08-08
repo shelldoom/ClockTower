61cbdaa45ba1dc4570f66dfd277ea341b41260a0
shelldoom <virtualized4ver@gmail.com>
2021-08-08 18:42:03 +0530

    formatting

--- logs/__init__.py
+++ logs/__init__.py
@@ -1,9 +1,11 @@
 import logging
 
 log_handlers = [logging.FileHandler("./logs/debug.log"), logging.StreamHandler()]
+
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
     handlers=log_handlers,
 )
+
 logger = logging.getLogger(__name__)
